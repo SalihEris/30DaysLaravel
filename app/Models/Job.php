598045ad357c57e14ac4b09935fdae007101320d
SalihEris <335385@student.mboutrecht.nl>
2024-10-21 14:53:05 +0200

    les 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace App\Models;
-php
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Job extends Model {
+    use HasFactory;
+
 protected $table= 'job_listings';
 
 protected $fillable = ['title', 'salary'];
+
+public function employer()
+{
+    return $this->belongsTo(Employer::class);
+}
 }
